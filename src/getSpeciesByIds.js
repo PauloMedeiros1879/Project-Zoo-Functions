c9db2309e27e3ec705544eec2e278ef0e25ae6b4
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-16 16:32:13 -0400

    Adiciona uma condiçao usando find para resolver o problema com retorno vazio da array

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...zooIds) {
-  // seu código aqui
-  if (typeof zooIds === 'undefined') {
-    return [];
-  }
-  return data.species.filter((specie) => specie.id === zooIds);
+  return data.species.filter((specie) => {
+    if (zooIds.find((element) => element === specie.id)) return specie;
+  });
 }
-
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
