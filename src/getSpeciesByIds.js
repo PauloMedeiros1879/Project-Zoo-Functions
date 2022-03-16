e85a1ebc06bcc28100d3bb6ba13727153b014a6c
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-16 16:41:19 -0400

    corrigindo o lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,6 +3,7 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...zooIds) {
   return data.species.filter((specie) => {
     if (zooIds.find((element) => element === specie.id)) return specie;
+    return '';
   });
 }
 console.log(getSpeciesByIds());
