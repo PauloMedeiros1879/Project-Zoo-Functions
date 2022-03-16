126fa5827cefb7440c09cc981188a2b7c02b6fde
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-16 19:01:44 -0400

    Tentativa do requisito 2, usei map com every, nao funcionou, tentei trocar map por reduce, so que me retorna false, e precisa ser true]

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const { species } = data;
+const { residents } = species;
 
+function getAnimalsOlderThan(animal, age) {
+  const verifyName = species.reduce((nameAnimal) => nameAnimal.name === animal);
+  if (verifyName === true) {
+    const verifyAge = residents.every((nameAge) => {
+      if (nameAge.age > age) return verifyAge;
+      return '';
+    });
+  }
+  return verifyName;
+}
 module.exports = getAnimalsOlderThan;
