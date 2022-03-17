b1f0d99cf724084f03ba61b2109075d9d79431d1
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-17 19:10:26 -0400

    Adiciona requisito 6, parte 1

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const visitorsZoo = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach(({ age } = entrants) => {
+    if (age < 18) {
+      visitorsZoo.child += 1;
+    }
+    if (age === 18 && age < 50) {
+      visitorsZoo.adult += 1;
+    }
+    if (age >= 50) {
+      visitorsZoo.senior += 1;
+    }
+  });
+  return visitorsZoo;
 }
 
 function calculateEntry(entrants) {
-  //  seu código aqui
+  // sono
 }
 
 module.exports = { calculateEntry, countEntrants };
