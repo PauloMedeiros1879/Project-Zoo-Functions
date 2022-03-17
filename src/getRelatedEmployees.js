f0b4fb6805a3cb44a9c8dc126b2fb057004c5622
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-17 12:09:02 -0400

    Tentando formular o requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((manager) => manager.managers
+    .some((manager2) => manager2 === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    return employees.filter((related) => related.managers.includes(managerId)
+      .find((related2) => related2 === managerId));
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
