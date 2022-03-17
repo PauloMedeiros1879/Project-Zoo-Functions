2cce720d0d72e83c42f0445ef60557a1e6cebf51
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-17 09:27:05 -0400

    Adiciona o requisito 3, tentei usar o filter, mas nao retornava o que eu queria, peguei a minha logica do primeiro requisito e refatorei para funcionar aqui

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  // return employees.filter((firstName, lastName) => {
+  if (!employeeName) return {}; // Apenas a linha 7 (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_NOT)
+  return employees.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
