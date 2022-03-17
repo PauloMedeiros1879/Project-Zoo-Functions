36e9622830cf265cdacc861655a0c61c71496fe8
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-17 15:48:15 -0400

    Requisito 5, adiciona com argumento

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -11,6 +11,14 @@ function countAnimals(animal) {
     });
     return result;
   }
+  // Com o argumento { specie: 'penguins' }, retorna um número, a quantidade de pinguins no zoológico;
+  // Com o argumento { specie: 'giraffes', sex: 'female' }, retorna um número, a quantidade de girafas do sexo feminino.
+  if (animal.sex) {
+    const test = species.find(({ name }) => name === animal.specie).residents;
+    const test2 = test.filter(({ sex }) => sex === animal.sex).length;
+    return test2;
+  }
+  return species.find(({ name }) => name === animal.specie).residents.length;
 }
 countAnimals();
 
