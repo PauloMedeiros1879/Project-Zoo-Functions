67146d0d96a18de5635bdc45c6fcd434f940a735
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-17 14:47:07 -0400

    Adiciona as zonas dos animais, mas falta fazer as restriçoes

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+
+function getAnimalMap(...options) {
+  if (!options) return {};
+  const animalsZone = ({
+    NE: species
+      .filter((specie) => specie.location === 'NE')
+      .map((animal) => animal.name),
+    NW: species
+      .filter((specie) => specie.location === 'NW')
+      .map((animal) => animal.name),
+    SE: species
+      .filter((specie) => specie.location === 'SE')
+      .map((animal) => animal.name),
+    SW: species
+      .filter((specie) => specie.location === 'SW')
+      .map((animal) => animal.name),
+  });
+  return animalsZone;
 }
 
 module.exports = getAnimalMap;
