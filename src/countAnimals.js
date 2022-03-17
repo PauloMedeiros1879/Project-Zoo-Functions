14a6cd2f5d8c9228b8088563f03930dff972831a
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-17 13:54:59 -0400

    Adiciona Requisito 5 parte sem o argumento

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  // Se nenhum argumento for passado, retorna um objeto cujo o nome de cada espécie é uma chave desse objeto, e o total de animais dessa espécie é o seu valor;
+  if (!animal) {
+    const result = {};
+    species.forEach(({ name, residents }) => {
+      result[name] = residents.length;
+    });
+    return result;
+  }
 }
+countAnimals();
 
 module.exports = countAnimals;
