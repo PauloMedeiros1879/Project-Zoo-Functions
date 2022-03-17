79e1f0d5493a131abc663757f44264c6a3d49710
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-17 18:28:35 -0400

    Adiciona o requisito 7, tentei de uma forma mais simples, nao deu certo e refiz toda a logica

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,21 +2,39 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-function getAnimalMap(...options) {
-  if (!options) return {};
-  const animalsZone = ({
-    NE: species
-      .filter((specie) => specie.location === 'NE')
-      .map((animal) => animal.name),
-    NW: species
-      .filter((specie) => specie.location === 'NW')
-      .map((animal) => animal.name),
-    SE: species
-      .filter((specie) => specie.location === 'SE')
-      .map((animal) => animal.name),
-    SW: species
-      .filter((specie) => specie.location === 'SW')
-      .map((animal) => animal.name),
+// function getAnimalMap(...options) {
+//   if (!options) return {};
+//   const animalsZone = ({
+//     NE: species
+//       .filter((specie) => specie.location === 'NE')
+//       .map((animal) => animal.name),
+//     NW: species
+//       .filter((specie) => specie.location === 'NW')
+//       .map((animal) => animal.name),
+//     SE: species
+//       .filter((specie) => specie.location === 'SE')
+//       .map((animal) => animal.name),
+//     SW: species
+//       .filter((specie) => specie.location === 'SW')
+//       .map((animal) => animal.name),
+//   });
+//   return animalsZone;
+// }
+
+function getAnimalMap({ includeNames, sex, sorted } = {}) {
+  const animalsZone = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((specie) => {
+    if (includeNames) {
+      const filter = specie.residents.filter((gener) => gener.sex === sex || sex === undefined);
+      const names = filter.map((gener2) => gener2.name);
+      const getNames = { [specie.name]: names };
+      if (sorted) {
+        names.sort();
+      }
+      animalsZone[specie.location].push(getNames);
+    } else {
+      animalsZone[specie.location].push(specie.name);
+    }
   });
   return animalsZone;
 }
