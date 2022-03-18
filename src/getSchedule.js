55ee986a65c2e9863252c6cc8436828f15069a13
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-18 09:48:43 -0400

    Requisito 8 tentei filtrar os animais com o horario de exibiçao, mas a exebiçao esta retornando array vaizo

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+// const { hours } = data;
+const { species } = data;
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  const days = {
+    exhibition: species.filter((element) => element.availability.includes(scheduleTarget))
+      .map((element) => element.name),
+  };
+  return days;
 }
 
 module.exports = getSchedule;
