b24616534b7d5403211d08afb42edaf8a41a4814
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-18 11:46:09 -0400

    Adiciona Requisito 9, usei .shift, tive que da uma pesquisada para tentar resolver

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const { species } = data;
+const { employees } = data;
+
+function getOldestFromFirstSpecies(id1) {
+  const animalsId = employees.filter(({ id }) => id === id1)
+    .map(({ responsibleFor }) => responsibleFor).shift(); // (https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/shift)
+  const animalsAndPeople = species.filter(({ id }) => id === animalsId[0]);
+  const animalsResidents = animalsAndPeople[0].residents;
+  const animalsOlds = animalsResidents.map(({ age }) => age)
+    .reduce((a, b) => Math.max(a, b), 0);
+  return Object.values(animalsResidents.find((specie) => specie.age === animalsOlds));
 }
 
 module.exports = getOldestFromFirstSpecies;
