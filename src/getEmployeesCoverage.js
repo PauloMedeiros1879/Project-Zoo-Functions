a01408f47f4990f2a5efb8f65b42bc3af33c18f1
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-18 18:24:06 -0400

    Adiciona requisito 10, tentei usar for of, mas da erro no lint, tentei outros meios, mas usei o tradicional for, finalizado com sucesso

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,8 +1,30 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { employees } = data;
+
+const information = () => employees.map(({ id, firstName, lastName, responsibleFor }) => {
+  const listObject = {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: [],
+    locations: [],
+  };
+  for (let i = 0; i < responsibleFor.length; i += 1) {
+    listObject.species.push(species.find((element) => element.id === responsibleFor[i]).name);
+    listObject.locations.push(species.find((element) => element.id === responsibleFor[i]).location);
+  }
+  return listObject;
+});
+
 function getEmployeesCoverage(element) {
-  if (!element) return element();
-  throw new Error('Informações inválidas');
+  if (!element) return information();
+
+  if (!information().some((e) => (e.id === element.id || e.fullName.includes(element.name)))) {
+    throw new Error('Informações inválidas');
+  }
+
+  return information().find((e) => (e.id === element.id || e.fullName.includes(element.name)));
 }
 
 module.exports = getEmployeesCoverage;
