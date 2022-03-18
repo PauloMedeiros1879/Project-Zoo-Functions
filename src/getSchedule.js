34b8b090fa9152962b5a72baf91dfefae85d86c9
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-18 11:20:42 -0400

    Adiciona algumas alteraçoes, fique travado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,14 +1,25 @@
 const data = require('../data/zoo_data');
 
-// const { hours } = data;
+const { hours } = data;
 const { species } = data;
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
-  const days = {
-    exhibition: species.filter((element) => element.availability.includes(scheduleTarget))
-      .map((element) => element.name),
+  const settingAlerts = {
+    Monday:
+    {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    },
   };
-  return days;
+  const days = scheduleTarget;
+  if (scheduleTarget === 'Monday') {
+    return settingAlerts;
+  }
+  return { [scheduleTarget]: {
+    officeHour: `Open from ${hours[days.open]}am until ${hours[days.close]}pm`,
+    exhibition: species.filter((element) => element.availability.includes(scheduleTarget))
+      .map((array) => array.name),
+  } };
 }
 
 module.exports = getSchedule;
